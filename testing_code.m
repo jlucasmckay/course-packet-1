735e141d6879763ab1b81aaf8a4d4780fc181c62
J. Lucas McKay PhD MSCR <j.lucas.mckay@gmail.com>
2020-11-06 08:13:01 -0500

    fixes to enable earlier matlab

--- testing_code.m
+++ testing_code.m
@@ -19,10 +19,28 @@ set(groot, 'DefaultFigureColor',[1 1 1])
 
 % load a sample trc file. don't forget the semicolon; it's a lot of data.
 % f = "study-data/trc-files/FOG-056/offmed-TUG-cognitive1-TP.trc";
-f = "offmed-TUG-standard1-TP.trc";
-d = read_trc(f);
+
+% set this flag to enable file i/o as binary .mat
+LEGACY = true;
+
+if ~LEGACY
+    % load a sample trc file. don't forget the semicolon; it's a lot of data.
+    f = 'offmed-TUG-standard1-TP.trc';
+    d = read_trc(f);
+else
+    % load a pre-loaded .mat file containing the trc data.
+    f = 'offmed-TUG-standard1-TP.mat';
+    d = read_mat('offmed-TUG-standard1-TP.mat');
+end
 
 % look at a small section
 peek(d)
 
-[R_HEEL_FOG, L_HEEL_FOG] = calculate_fog_JLM(f,true);
+% if the LEGACY flag has been set, send the dataset to the calculate_fog_JLM method.
+% otherwise send the name of the .trc file.
+
+if ~LEGACY
+    [R_HEEL_FOG, L_HEEL_FOG] = calculate_fog_JLM(f,true);
+else
+    [R_HEEL_FOG, L_HEEL_FOG] = calculate_fog_JLM(d,true);
+end
